c7f2b3e931e2cc73ccf12955e132639ea3d661dd
Gloriyaseelan <101190648+Gloriyaseelan@users.noreply.github.com>
2022-03-21 17:02:25 +0530

    Add files via upload

--- employee.cs
+++ employee.cs
@@ -1,58 +1,50 @@
-using System;
-class employee
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace ConsoleApp1
 {
-public void details()
-{
-Console.WriteLine("Enter employee id: ");
-string id=Console.ReadLine();
-Console.WriteLine("Enter your name: ");
-string name=Console.ReadLine();
-Console.WriteLine("Enter male/female: ");
-string gender=Console.ReadLine();
-Console.WriteLine("enter year of experiance: ");
-int yox=Convert.ToInt32(Console.ReadLine());
-
-}
-}
-
-
-class salary:employee
-{
-public void calci()
-{
-Console.WriteLine("enter salary: ");
-int salary=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("enter your department (sales/production): ");
-string dept=Console.ReadLine();
-if(dept=="sales")
-{
-int total=((salary*20)/100)+salary;
-Console.WriteLine("TOTAL SALARY="+" "+total);
-}
-else
-{
-int total=((salary*10)/100)+salary;
-Console.WriteLine("TOTAL SALARY="+" "+total);
-}
-}
-}
-
+    internal class employee
+    {
+        public static void Main()
+        {
+            {
+                FileStream fs = new FileStream
+                        ("E:\\gloriya\\program\\console.txt",
+                        FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                BinaryWriter bw = new BinaryWriter(fs);
+                BinaryWriter bw1 = new BinaryWriter(fs);
+                BinaryWriter bw2 = new BinaryWriter(fs);
+                BinaryWriter bw3 = new BinaryWriter(fs);
+                bw.Write("gulab jamun");
+                bw1.Write("brownnie ");
+                bw2.Write("icecream");
+                bw2.Write("choco cake");
+                bw.Flush();
+                fs.Close();
+                FileStream fs1 = new FileStream(@"E:\gloriya\program\console.txt", FileMode.Open, FileAccess.ReadWrite);
+                BinaryReader br = new BinaryReader(fs1);
+                BinaryReader br1 = new BinaryReader(fs1);
+                BinaryReader br2 = new BinaryReader(fs1);
+                BinaryReader br3 = new BinaryReader(fs1);
+                string str = br.ReadString();
+                string str1 = br1.ReadString();
+                string str2 = br2.ReadString();
+                string str3 = br3.ReadString();
+                Console.WriteLine(str);
+                Console.WriteLine(str1);
+                Console.WriteLine(str2);
+                Console.WriteLine(str3);
+                fs1.Close();
+                Console.Read();
+            }
 
 
-
-class inherit
-{
-public static void Main()
-{
-salary obj=new salary();
-obj.details();
-obj.calci();
-}
-}
-
-
-
-
-
-
+        }
+    }
+    }
 

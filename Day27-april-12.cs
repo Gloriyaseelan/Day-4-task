2f992bd68fcc660414989be7f1deaa33a106b6e2
Gloriyaseelan <101190648+Gloriyaseelan@users.noreply.github.com>
2022-04-12 22:01:42 +0530

    Update and rename Day27-april12.cs to Day27-april-12.cs

--- Day27-april-12.cs
+++ Day27-april-12.cs
@@ -5,7 +5,7 @@ using System.Data.SqlClient;
 
 namespace Dataa
 {
-    class Day27_april12
+     class Day27_April_12
     {
         public void create()
         {
@@ -13,23 +13,22 @@ namespace Dataa
 
             SqlConnection con = new SqlConnection("data source = GUNA; database = trail;user id =sa; password=p@ssw0rd");
             con.Open();
-            SqlCommand cmd = new SqlCommand("create table Enrollment (sno int, classname varchar(20))", con);
+            SqlCommand cmd = new SqlCommand("create table Enrollment2 (sno int,age int,firstname varchar(20))", con);
             cmd.ExecuteNonQuery();
             Console.WriteLine("Table Created");
             con.Close();
 
 
         }
-
         public void Insert()
         {
-            SqlConnection con = new SqlConnection("data source = GUNA; database = trail;user id =sa; password=p@ssw0rd");
-            string sqlqry = "insert Enrollment values(1 ,'Agri')";
-            SqlCommand cmd = new SqlCommand(sqlqry, con);
-            con.Open();
-            cmd.ExecuteNonQuery();
-            con.Close();
-            Console.WriteLine("row inserted");
+           SqlConnection con = new SqlConnection("data source = GUNA; database = trail;user id =sa; password=p@ssw0rd");
+           string sqlqry = "insert Enrollment2 values(1,22 ,'Ammu')";
+           SqlCommand cmd = new SqlCommand(sqlqry, con);
+           con.Open();
+           cmd.ExecuteNonQuery();
+           con.Close();
+           Console.WriteLine("row inserted");
 
         }
 
@@ -37,19 +36,19 @@ namespace Dataa
         {
             SqlConnection con = new SqlConnection("data source =GUNA ;database= Trail;user id = sa;password=p@ssw0rd");
 
-            string sqlqry = "update Enrollment set classname='biology' where Sno=1";
+            string sqlqry = "update Enrollment2 set firstname='biology' where Sno=1";
             SqlCommand cmd = new SqlCommand(sqlqry, con);
             con.Open();
             cmd.ExecuteNonQuery();
             con.Close();
-            Console.WriteLine("Row Updated");
+           Console.WriteLine("Row Updated");
         }
 
         public void delete()
         {
             SqlConnection con = new SqlConnection("data source =GUNA ;database= Trail;user id = sa;password=p@ssw0rd");
 
-            string sqlqry = "delete Enrollment where Sno=1";
+            string sqlqry = "delete Enrollment2 where Sno=1";
             SqlCommand cmd = new SqlCommand(sqlqry, con);
             con.Open();
             cmd.ExecuteNonQuery();
@@ -58,13 +57,13 @@ namespace Dataa
         }
         public static void Main()
         {
-            Day27_april12 Eobj = new Day27_april12();
+            Day27_April_12 Eobj = new Day27_April_12();
 
-            Eobj.Insert();
-            //Eobj.delete();
+            //Eobj.Insert();
             //Eobj.update();
-
+            Eobj.delete();
         }
+
     }
 }
 
